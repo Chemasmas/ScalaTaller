d9c970530a89db74f7b29f9edc065bbf509b410a
Chemasmas <eva.01.2015@gmail.com>
2016-04-18 22:22:06 -0500

    Flags Added

--- entorno.sh
+++ entorno.sh
@@ -2,11 +2,12 @@
 #URLS JDKURL
 URLJDK32="http://download.oracle.com/otn-pub/java/jdk/8u77-b03/jdk-8u77-linux-i586.tar.gz";
 URLJDK64="http://download.oracle.com/otn-pub/java/jdk/8u77-b03/jdk-8u77-linux-x64.tar.gz"
+FLAGS='--no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie"'
 echo "Descargando JAVA";
 arq=$(/bin/uname -m);
-if [ $arq -eq "i686" ]; then
+if [ $arq == "i686" ]; then
   URL=$URLJDK32;
 else
   URL=$URLJDK64;
 fi
-/bin/usr/wget $URL -O "JDk.tar.gz";
+/usr/bin/wget $FLAGS $URL ;
