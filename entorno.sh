15935e5b7527ed633d768f85414863877e203697
Chemasmas <eva.01.2015@gmail.com>
2016-04-18 22:57:20 -0500

    Descompresion y mv

--- entorno.sh
+++ entorno.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #URLS JDKURL
-URLJDK32="http://download.oracle.com/otn-pub/java/jdk/8u77-b03/jdk-8u77-linux-i586.tar.gz";
+URLJDK32="http://download.oracle.com/otn-pub/java/jdk/8u77-b03/jdk-8u77-linux-i586.tar.gz"
 URLJDK64="http://download.oracle.com/otn-pub/java/jdk/8u77-b03/jdk-8u77-linux-x64.tar.gz"
-FLAGS='--no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie"'
-echo "Descargando JAVA";
-arq=$(/bin/uname -m);
+echo "Descargando JAVA"
+arq=$(/bin/uname -m)
 if [ $arq == "i686" ]; then
-  URL=$URLJDK32;
+  URL=$URLJDK32
 else
-  URL=$URLJDK64;
+  URL=$URLJDK64
 fi
-/usr/bin/wget $FLAGS $URL ;
+/usr/bin/wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" $URL -O JDK.tar.gz
+/bin/tar -xvzf ./JDK.tar.gz /usr/lib/jvm
