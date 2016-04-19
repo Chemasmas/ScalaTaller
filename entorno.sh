7a4392f74614eb2e4854d965159b004e19e3f369
Chemasmas <eva.01.2015@gmail.com>
2016-04-19 17:28:13 -0500

    RENAME y PWD

--- entorno.sh
+++ entorno.sh
@@ -2,6 +2,8 @@
 #URLS JDKURL
 URLJDK32="http://download.oracle.com/otn-pub/java/jdk/8u77-b03/jdk-8u77-linux-i586.tar.gz"
 URLJDK64="http://download.oracle.com/otn-pub/java/jdk/8u77-b03/jdk-8u77-linux-x64.tar.gz"
+CARPETAO="jdk1.8.0_77"
+CARPETAD="jvm"
 echo "Descargando JAVA"
 arq=$(/bin/uname -m)
 if [ $arq == "i686" ]; then
@@ -10,4 +12,9 @@ else
   URL=$URLJDK64
 fi
 /usr/bin/wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" $URL -O JDK.tar.gz
-/bin/tar -xvzf ./JDK.tar.gz
+/bin/tar -xzf ./JDK.tar.gz
+mv ./$CARPETAO ./$CARPETAD
+cd ./$CARPETAD
+CURRDIRR=$(pwd);
+cd ..
+echo $CURRDIRR
