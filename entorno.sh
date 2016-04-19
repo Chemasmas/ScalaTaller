08f530d57982fa75f8b7fe701f43469ec0f20412
Chemasmas <eva.01.2015@gmail.com>
2016-04-18 22:08:47 -0500

    Condicional Bash

--- entorno.sh
+++ entorno.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-#$JDKURL =
-
+#URLS JDKURL
+URLJDK32="http://download.oracle.com/otn-pub/java/jdk/8u77-b03/jdk-8u77-linux-i586.tar.gz";
+URLJDK64="http://download.oracle.com/otn-pub/java/jdk/8u77-b03/jdk-8u77-linux-x64.tar.gz"
 echo "Descargando JAVA";
-arq = 'uname -m';
-echo $arq;
+arq=$(/bin/uname -m);
+if [ $arq -eq "i686" ]; then
+  URL=$URLJDK32;
+else
+  URL=$URLJDK64;
+fi
+/bin/usr/wget $URL -O "JDk.tar.gz";
