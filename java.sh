6935bf67c868d25031c4f313076e0a870ebd1b43
Chemasmas <eva.01.2015@gmail.com>
2016-04-20 00:08:04 -0500

    SCALA FINISH, ADDED SBT

--- java.sh
+++ java.sh
@@ -3,10 +3,14 @@
 URLJDK32="http://download.oracle.com/otn-pub/java/jdk/8u77-b03/jdk-8u77-linux-i586.tar.gz"
 URLJDK64="http://download.oracle.com/otn-pub/java/jdk/8u77-b03/jdk-8u77-linux-x64.tar.gz"
 URLSCALA="http://downloads.lightbend.com/scala/2.11.8/scala-2.11.8.tgz"
+URLSBT="https://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.11/sbt-launch.jar?_ga=1.35464034.2128109674.1461125401"
 CARPETAO="jdk1.8.0_77"
 CARPETAD="jdk8"
+CARPETAOS="scala-2.11.8"
+CARPETADS="scala"
 echo "Descargando JAVA"
 JVMHOME=/usr/lib/$CARPETAD
+SCALAHOME=/usr/local/share/$CARPETADS
 arq=$(/bin/uname -m)
 if [ $arq == "i686" ]; then
   URL=$URLJDK32
@@ -22,10 +26,18 @@ cd ..
 sudo mv $CURRDIR $JVMHOME
 echo export J2SDKDIR=$JVMHOME >> $HOME/.bashrc
 echo export J2REDIR=$JVMHOME/jre >> $HOME/.bashrc
-echo export PATH=$PATH:$JVMHOME/bin:$JVMHOME/db/bin:$JVMHOME/jre/bin >> $HOME/.bashrc
 echo export JAVA_HOME=$JVMHOME >> $HOME/.bashrc
 echo export DERBY_HOME=$JVMHOME/db >> $HOME/.bashrc
 echo "Terminado JAVA"
 echo "Descargando SCALA"
-/usr/bin/wget $SCALA -O scala.tgz
+/usr/bin/wget $URLSCALA -O scala.tgz
 /bin/tar -xzf ./scala.tgz
+mv ./$CARPETAOS ./$CARPETADS
+cd ./$CARPETADS
+CURRDIR=$(pwd);
+cd ..
+sudo mv $CURRDIR $SCALAHOME
+echo "Terminado SCALA"
+/usr/bin/wget $URLSBT
+#Edicion del PATH
+echo export PATH=$PATH:$JVMHOME/bin:$JVMHOME/db/bin:$JVMHOME/jre/bin:$SCALAHOME/bin >> $HOME/.bashrc
